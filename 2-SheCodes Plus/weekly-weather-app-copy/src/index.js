89fdf3e9706495bad759df43966bf617e8117365
Lorraine Thomas <thomas.lorraine1@gmail.com>
2024-04-21 15:20:50 -0400

    Include function for each day in forecast to change weather elements

--- 2-SheCodes Plus/weekly-weather-app-copy/src/index.js
+++ 2-SheCodes Plus/weekly-weather-app-copy/src/index.js
@@ -1,24 +1,22 @@
 function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
+    let temperatureValue = Math.round(convertTemperature(response.data.temperature.current));
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind-speed");
     timeElement = document.querySelector(".time");
+    let dayTime = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
 
-
-    let temperatureValue = Math.round(convertTemperature(response.data.temperature.current));
-    let speed = Math.round(response.data.wind.speed * 1.609344);
-    let dayTime = new Date(response.data.time * 1000);
-    
-    temperatureElement.innerHTML = temperatureValue;
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = `${days[dayTime.getDay()]} ${dayTime.getHours()}:${minutes}`;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windElement.innerHTML = speed;
-    timeElement.innerHTML = `${days[dayTime.getDay()]} ${dayTime.getHours()}:${minutes}`;
+    temperatureElement.innerHTML = temperatureValue;
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+    let speed = Math.round(response.data.wind.speed * 1.609344); 
 
     let days = [
         "Sunday",
@@ -47,11 +45,31 @@ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api
 axios.get(apiUrl).then(refreshWeather);
 }
 
-
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     searchCity(searchInput.value);
+}
+
+function displayForecast() {
+    let days = ["Sat", "Sun", "Mon", "Tues", "Wed"];
+    let forecastHtml = "";
+
+    days.forEach(function (day) {
+        forecastHtml = 
+            forecastHtml + 
+        `
+            <div class="weather-forecast-day">
+                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-icon">🌤️</div>
+                <div class="weather-forecast-temperature">
+                    <strong>67°</strong>
+                </div>
+                <div class="weather-forecast-temperature">61°</div>
+                </div>
+            </div>
+            `;
+    }
 }
 
 let searchFormElement = document.querySelector("#search-form");
