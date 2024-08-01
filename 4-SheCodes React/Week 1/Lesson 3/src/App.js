7a35de5b774b29f209ef1347c960b92905d5954f
Lorraine Thomas <thomas.lorraine1@gmail.com>
2024-08-01 15:53:15 -0400

    React week 1 lesson 3 codesandbox code

---
+++ 4-SheCodes React/Week 1/Lesson 3/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+    return(
+    <div className="App">
+        <h1>My Awesome weather app</h1>
+        <h2>Built with React</h2>
+    </div>
+    );
+}
