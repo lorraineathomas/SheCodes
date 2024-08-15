1e3b2853e95c4881f7e741cd5ea4fb97cfc8b822
Lorraine Thomas <thomas.lorraine1@gmail.com>
2024-08-15 13:44:24 -0400

    Start of Lesson 1 course video code

---
+++ 4-SheCodes React/Week 4/Lesson 1/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactAnimatedWeather from "react-animated-weather";
+
+import "./App.css";
+
+function App() {
+    return <h1>Hello from React</h1>
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
